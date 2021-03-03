26461ba358d3262114cd55686f3d3038c7acd0fb
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2021-03-03 15:41:08 +0100

    Add carousel

---
+++ JS/Home.js
@@ -0,0 +1,26 @@
+//carousel photo 
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel({
+        interval : 3000
+    });
+      
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+      $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+      $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+      $("#myCarousel").carousel(2);
+    });
+      
+    // Enable Carousel Controls
+    // $(".carousel-control-prev").click(function(){
+    //   $("#myCarousel").carousel("prev");
+    // });
+    // $(".carousel-control-next").click(function(){
+    //   $("#myCarousel").carousel("next");
+    // });
+  });
